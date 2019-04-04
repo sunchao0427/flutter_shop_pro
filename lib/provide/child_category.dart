ab23e862b4a3ae2655d3ac5e0ec256c1460f0592
sunchao0427 <sunchao0427@qq.com>
2019-04-04 17:28:46 +0800

    fix 修复bug三个

--- lib/provide/child_category.dart
+++ lib/provide/child_category.dart
@@ -4,8 +4,19 @@ import '../model/Category.dart';
 class ChildCategory with ChangeNotifier{
   List<BxMallSubDto> childCategoryList = [];
 
-  getChildCategory(List list){
-    childCategoryList = list;
+  getChildCategory(List<BxMallSubDto> list){
+
+    BxMallSubDto all = BxMallSubDto();
+
+    all.mallSubId = '00';
+    all.mallSubName = '全部';
+    all.comments = 'null';
+    all.mallCategoryId = '00';
+
+  childCategoryList = [all];
+  childCategoryList.addAll(list);
+
+    // childCategoryList = list;
     notifyListeners();
   }
 }
