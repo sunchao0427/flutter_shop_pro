d89828369d1e28ca8b473124250c1406edcc0c2b
sunchao0427 <sunchao0427@qq.com>
2019-04-07 20:30:25 +0800

    add 写总体路由及配置  静态化路由处理

--- lib/routers/routers.dart
+++ lib/routers/routers.dart
@@ -1,3 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:fluro/fluro.dart';
 import './details_router.dart';
+
+class Routes{
+  static String root = '/';
+  static String detailsPage = '/detail';
+  
+  static void configureRoutes(Router router){
+    router.notFoundHandler = new Handler(
+      handlerFunc: (BuildContext context,Map<String,List<String>> params){
+        print('ERROR ======>');
+      }
+    );
+    
+    router.define(detailsPage, handler: detailsHandler);
+  }
+
+}
