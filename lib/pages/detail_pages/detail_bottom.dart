d61cb0ab3208d053f91964b96f0559cfca6baeb4
sunchao0427 <sunchao0427@qq.com>
2019-04-08 23:14:32 +0800

    add 添加详情页底部购买栏，使用Stack内部嵌套Positioned()方法

---
+++ lib/pages/detail_pages/detail_bottom.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class DetailBottom extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: ScreenUtil().setWidth(750),
+      height: ScreenUtil().setHeight(80),
+      color: Colors.white,
+      child: Row(
+        children: <Widget>[
+          InkWell(
+            onTap: (){
+
+            },
+            child: Container(
+              width: ScreenUtil().setWidth(110),
+              alignment: Alignment.center,
+              child: Icon(
+                Icons.shopping_cart,
+                size: 35,
+                color: Colors.red,
+                ),
+            ),
+          ),
+          InkWell(
+            onTap: (){
+
+            },
+            child: Container(
+              width: ScreenUtil().setWidth(320),
+              height: ScreenUtil().setHeight(80),
+              alignment: Alignment.center,
+              color: Colors.green[600],
+              child: Text(
+                '加入购物车',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: ScreenUtil().setSp(28)
+                ),
+              ),
+            ),
+          ),
+          InkWell(
+            onTap: (){
+
+            },
+            child: Container(
+              width: ScreenUtil().setWidth(320),
+              height: ScreenUtil().setHeight(80),
+              alignment: Alignment.center,
+              color: Colors.pink,
+              child: Text(
+                '立即购买',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: ScreenUtil().setSp(28)
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      
+    );
+  }
+}
--- lib/pages/details_page.dart
+++ lib/pages/details_page.dart
@@ -5,52 +5,56 @@ import './detail_pages/detail_top_area.dart';
 import './detail_pages/detail_explain.dart';
 import './detail_pages/detail_tabBar.dart';
 import './detail_pages/detail_web_detail.dart';
-
+import './detail_pages/detail_bottom.dart';
 
 class DetailPage extends StatelessWidget {
-
   final String goodsId;
   DetailPage(this.goodsId);
 
   @override
   Widget build(BuildContext context) {
-    
     return Container(
-      child: Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back),
-            onPressed: (){
-              Navigator.pop(context);
-            },
-          ),
-          title: Text('商品详细信息'),
-        ),
-        body: FutureBuilder(
-          future: _getGoodInfo(context),
-          builder: (context,snapshot){
-            if(snapshot.hasData){
-              return Container(
-                child: ListView(
-                  children: <Widget>[
-                    DetailTopArea(),
-                    DetailExplainArea(),
-                    DetailTabBar(),
-                    DetailWebDetail()
-                  ],
-                ),
-              );
-            }else{
-              return Text('加载中........');
-            }
-          },
-        )
-      )
-    );
+        child: Scaffold(
+            appBar: AppBar(
+              leading: IconButton(
+                icon: Icon(Icons.arrow_back),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              title: Text('商品详细信息'),
+            ),
+            body: FutureBuilder(
+              future: _getGoodInfo(context),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Stack(
+                    children: <Widget>[
+                      Container(
+                        child: ListView(
+                          children: <Widget>[
+                            DetailTopArea(),
+                            DetailExplainArea(),
+                            DetailTabBar(),
+                            DetailWebDetail()
+                          ],
+                        ),
+                      ),
+                      Positioned(
+                        bottom: 0,
+                        left: 0,
+                        child: DetailBottom(),
+                      ),
+                    ],
+                  );
+                } else {
+                  return Text('加载中........');
+                }
+              },
+            )));
   }
 
-  Future _getGoodInfo(BuildContext context)async{
-
+  Future _getGoodInfo(BuildContext context) async {
     await Provide.value<DetailInfoProvide>(context).getGoodsInfo(goodsId);
     return '加载完成';
   }
