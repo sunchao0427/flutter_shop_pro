0f41c03ee1104f433924911767e31b8ab95c902f
sunchao0427 <sunchao0427@qq.com>
2019-04-16 08:20:48 +0800

    fix 修改server_url地址为新地址

--- lib/config/service_url.dart
+++ lib/config/service_url.dart
@@ -1,4 +1,4 @@
-const serviceUrl = 'http://test.baixingliangfan.cn/baixing/';
+const serviceUrl = 'http://v.jspang.com:8088/baixing/';
 const servicePath = {
   'homePageContent' : serviceUrl+'wxmini/homePageContent',//商品首页内容
   'homePageBelowConten' :serviceUrl + 'wxmini/homePageBelowConten', //火爆专区接口 
